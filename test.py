483f3e12d97c4f50c724690285261c492afedcb5
Kedarnath Peraka <71390186+kedarnathdev@users.noreply.github.com>
2023-02-17 17:55:20 +0000

    update

--- test.py
+++
@@ -1,9 +0,0 @@
-from pytube import YouTube
-from pyrogram.types import InlineKeyboardButton as IKB, InlineKeyboardMarkup as IKM
-
-url = "https://youtu.be/YpJYSIRa03Q"
-
-yt = YouTube(url)
-y = yt.streams.filter(only_audio=True)
-for i in y:
-    print(i.abr)
