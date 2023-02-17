36414ebefeeea1ed14604b066ef4ef3a1484ca7d
Kedarnath Peraka <71390186+kedarnathdev@users.noreply.github.com>
2023-02-17 11:55:06 +0000

    upating app

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from pytube import YouTube
 from pyrogram import Client, filters
+from dotenv import load_dotenv
 import os
 
+
+load_dotenv()
 bot = Client(
-    name=os.environ.get('NAME'),
-    api_id=os.environ.get('API_ID'),
-    api_hash=os.environ.get('API_HASH'),
-    bot_token=os.environ.get('BOT_TOKEN')
+    name=os.getenv('NAME'),
+    api_id=os.getenv('API_ID'),
+    api_hash=os.getenv('API_HASH'),
+    bot_token=os.getenv('BOT_TOKEN')
 )
 
-bot.on_message(filters.command(['start']))
-def start(client, message):
+@bot.on_message(filters.private)
+def handle_message(client, message):
     bot.send_message(chat_id=message.chat.id, text="Welcome! to ytdownloadertgbot.")
+
+
+
+bot.run()
