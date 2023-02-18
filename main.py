d599c70c8eedba35bc93b41947d68c0e825949a3
Kedarnath Peraka <71390186+kedarnathdev@users.noreply.github.com>
2023-02-18 09:08:03 +0000

    update variable global url

--- main.py
+++ main.py
@@ -41,29 +41,29 @@ def handle_message(client, message):
 
 @bot.on_message(filters.private)
 def handle_urls(client, message):
-    global msg
-    msg = message
+    global url
+    url = message
     bot.send_message(chat_id=message.chat.id, text="Select: ", reply_markup=keyboardAV)
 
 @bot.on_callback_query()
 def handle_callback(client, callbackData):
     if callbackData.data == "video":
-        fetchVideoQualities(message=msg)
+        fetchVideoQualities(message=url)
     elif callbackData.data == "audio":
-        fetchAudioQualities(message=msg)
+        fetchAudioQualities(message=url)
     else:
-        yt = YouTube(msg.text)
+        yt = YouTube(url.text)
         y = yt.streams.get_by_itag(int(callbackData.data))
         if y.mime_type == 'video/mp4':
             wget.download(yt.thumbnail_url, out="thumb.jpg")
             y.download(output_path=os.getcwd(), filename="video.mp4")
-            bot.send_video(chat_id=msg.chat.id, video=open("video.mp4", "rb"), thumb="thumb.jpg", duration=yt.length, file_name=y.default_filename, supports_streaming=True)
+            bot.send_video(chat_id=url.chat.id, video=open("video.mp4", "rb"), thumb="thumb.jpg", duration=yt.length, file_name=y.default_filename, supports_streaming=True)
             os.remove("video.mp4")
             os.remove("thumb.jpg")
         else:
             wget.download(yt.thumbnail_url, out="thumb.jpg")
             y.download(output_path=os.getcwd(), filename="music.mp3")
-            bot.send_audio(chat_id=msg.chat.id, audio=open("music.mp3", "rb"), file_name=y.default_filename[:len(y.default_filename)-3]+'.mp3', duration=yt.length, thumb="thumb.jpg")
+            bot.send_audio(chat_id=url.chat.id, audio=open("music.mp3", "rb"), file_name=y.default_filename[:len(y.default_filename)-3]+'.mp3', duration=yt.length, thumb="thumb.jpg")
             os.remove("music.mp3")
             os.remove("thumb.jpg")
 
